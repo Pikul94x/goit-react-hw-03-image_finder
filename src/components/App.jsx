3087fd8f810f09a117499b298346d49b00a11774
Jakub Pikul <jakub.pikul1994@gmail.com>
2023-03-16 15:02:48 +0100

    searchbar component done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,33 @@
+import { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 
-export const App = () => {
-  return (
-    <div>
-      <Searchbar />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    images: [],
+    searchQuery: '',
+    page: 1,
+    isLoading: false,
+    error: null,
+    foundImages: null,
+    currentLargeImg: null,
+  };
+
+  setInitialParams = searchQuery => {
+    if (searchQuery === '') {
+      return alert(`Enter the search value!`);
+    }
+    if (searchQuery === this.state.searchQuery) {
+      return;
+    }
+
+    this.setState({ images: [], searchQuery, page: 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.setInitialParams} />
+      </div>
+    );
+  }
+}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,11 +1,47 @@
-import { Component } from "react";
+import { Component } from 'react';
+
+const INITIAL_STATE = {
+  searchQuery: '',
+};
 
 class Searchbar extends Component {
-    render() {
-        return (
-            <></>
-        )
-    }
+  state = { ...INITIAL_STATE };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.searchQuery);
+    this.setState({ ...INITIAL_STATE });
+  };
+  render() {
+    const { searchQuery } = this.state;
+    return (
+      <>
+        <header className="searchbar">
+          <form className="form" onSubmit={this.handleSubmit}>
+            <button type="submit" className="button">
+              <span className="button-label">Search</span>
+            </button>
+
+            <input
+              onChange={this.handleChange}
+              value={searchQuery}
+              name="searchQuery"
+              className="input"
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+            />
+          </form>
+        </header>
+      </>
+    );
+  }
 }
 
-export default Searchbar
+export default Searchbar;
